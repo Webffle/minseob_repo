ceccb44491c4d960614db6c8e0279743a44976c8
striker1826 <alstjq123579@daum.net>
2023-11-30 15:27:26 +0900

    first commit

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+export const data = [
+  {
+    id: 1,
+    title: '에이윈더 공식몰 멤버쉽 혜택',
+    createdAt: '2021.08.05',
+  },
+  {
+    id: 1,
+    title: '에이윈더 공식몰 멤버쉽 혜택',
+    createdAt: '2021.08.05',
+  },
+];
