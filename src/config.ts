5cd17cb2653cd84b628a6a3e7de3524295f626e8
striker1826 <alstjq123579@daum.net>
2023-11-30 16:24:53 +0900

    feat: getAllNotice

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,23 @@ export const data = [
     createdAt: '2021.08.05',
   },
   {
-    id: 1,
-    title: '에이윈더 공식몰 멤버쉽 혜택',
-    createdAt: '2021.08.05',
+    id: 2,
+    title: '롯데택배 배송 불가 지역 안내',
+    createdAt: '2022.01.11',
+  },
+  {
+    id: 3,
+    title: '토비콤 x 트리플래닛 포토후기 이벤트 당첨자 발표',
+    createdAt: '2021.10.29',
+  },
+  {
+    id: 4,
+    title: '[오픈페스타] 포토후기 이벤트 당첨자 발표',
+    createdAt: '2021.10.14',
+  },
+  {
+    id: 5,
+    title: '현님 화이팅',
+    createdAt: '2023.11.30',
   },
 ];
